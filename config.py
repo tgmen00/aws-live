c781f7007093e1d5a3ee2b329823bcffb6be9ef0
Tam Ga Men <75972896+tgmen00@users.noreply.github.com>
2023-08-28 11:49:55 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.czdqsxrgcpks.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "tamgamen-employee"
+customregion = "us-east-1"
+
